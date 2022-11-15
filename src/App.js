533e8885ec65c0e33287b316ee43f5e85760ca58
Alisha Saxena <alishasaxena2016@gmail.com>
2022-11-15 12:20:07 +0530

    implemented loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ChooseFiles from "./ChooseFiles";
 import Header from "./Header.js";
+import Loader from ".././src/Loader/index";
 import PDFViewer from "./PDFViewer";
 import ResultViewer from "./ResultViewer";
 
@@ -9,20 +10,25 @@ function App() {
   const [viewerInstance, setViewerInstance] = useState();
   const [files, setFiles] = useState({});
   const [selectedFileData, setSelectedFileData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleSelectInstanceFile = (file) => {
     console.log(file, "hjiujhjikjhjikj fileee");
     viewerInstance.UI.loadDocument(file);
   };
 
-    useEffect(()=> {
-        // debugger
-        if(Object.keys(files).length && viewerInstance){
-            handleSelectInstanceFile(files.questions[0]?.fileUrl)
-            console.log("files", files.questions[0]?.fileUrl)
-        }
-    }, [viewerInstance, files])
+  useEffect(() => {
+    // debugger
+    if (Object.keys(files).length && viewerInstance) {
+      handleSelectInstanceFile(files.questions[0]?.fileUrl);
+      console.log("files", files.questions[0]?.fileUrl);
+    }
+  }, [viewerInstance, files]);
   console.log(files, "heyyyooooooooooooo");
+
+  if (!isLoading) {
+    return <Loader />;
+  }
 
   return (
     <div className="app">
