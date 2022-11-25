5aa38d8eed5eadc45368c2fbedeef0927371dc04
Shah Ikbal Ahmed <76581633+shahikbalahmed@users.noreply.github.com>
2022-11-25 19:34:51 +0530

    base url changed

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://devtutorbin.mrikal.com/";
+export const BASE_URL = "https://questionparser-backend.tutorbin.com/";
 // export const BASE_URL = "https://722f-14-143-254-82.ngrok.io/";
