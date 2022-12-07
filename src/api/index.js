3ca86e96a7b74f8eea509ac88291727408d9638e
Shah Ikbal Ahmed <76581633+shahikbalahmed@users.noreply.github.com>
2022-12-07 17:33:48 +0530

    base url changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
-// export const BASE_URL = "https://questionparser-backend.tutorbin.com/";
-export const BASE_URL = "https://devtutorbin.mrikal.com/";
-export const IMAGE_URL = "https://tutorbin-images.s3.ap-south-1.amazonaws.com";
+export const BASE_URL = "https://questionparser-backend.tutorbin.com/";
+// export const BASE_URL = "https://devtutorbin.mrikal.com/";
+// export const IMAGE_URL = "https://tutorbin-images.s3.ap-south-1.amazonaws.com";
+export const IMAGE_URL = "https://lib-parser-img.s3.ap-south-1.amazonaws.com";
